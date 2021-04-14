75bf942e2eaeccb2345c6cebb50fbd4245709008
Jaime Pignatelli <jaimepignatelli@Jaimes-Air.lan>
2021-04-14 18:22:44 +0100

    hello

--- 6_Music_and_sport_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="6_Music_and_sport_32x32" tilewidth="32" tileheight="32" tilecount="464" columns="16">
- <image source="6_Music_and_sport_32x32.png" width="512" height="928"/>
-</tileset>
