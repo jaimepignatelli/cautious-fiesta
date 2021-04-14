54cba3381485c85ebd5d6a4ee3e1e1b73a6f7bf9
Jaime Pignatelli <jaimepignatelli@Jaimes-Air.lan>
2021-04-14 17:29:26 +0100

    hello

---
+++ 2_LivingRoom_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="2_LivingRoom_16x16" tilewidth="32" tileheight="32" tilecount="176" columns="8">
+ <image source="../Modern_Interiors/1_Interiors/16x16/Theme_Sorter/2_LivingRoom_16x16.png" width="256" height="720"/>
+</tileset>
---
+++ Modern.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Modern" tilewidth="32" tileheight="32" tilecount="192" columns="12">
+ <image source="../modern_edits_for_rpg_maker_vx_ace_by_milkian_dbvyco2/!modern-edits-doors.png" width="384" height="512"/>
+</tileset>
